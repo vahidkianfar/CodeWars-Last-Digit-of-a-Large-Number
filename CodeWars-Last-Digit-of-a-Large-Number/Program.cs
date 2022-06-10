09c8448114a83d841df6386070ede57e35a088f0
vahidkianfar <vahidkianfar@gmail.com>
2022-06-10 22:09:23 +0100

    Added Lyric!

--- CodeWars-Last-Digit-of-a-Large-Number/Program.cs
+++ CodeWars-Last-Digit-of-a-Large-Number/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine();
+Console.WriteLine(@"Karma police, arrest this man
+He talks in maths, he buzzes like a fridge
+He's like a detuned radio
+
+Karma police, arrest this girl
+Her Hitler hairdo is making me feel ill
+And we have crashed her party");
