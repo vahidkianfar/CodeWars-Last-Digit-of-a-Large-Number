5e1a9d0563079b373d01097641b66535454fe987
vahidkianfar <vahidkianfar@gmail.com>
2022-06-10 22:11:28 +0100

    Added Very useful comment!

--- CodeWars-Last-Digit-of-a-Large-Number/LastDigit.cs
+++ CodeWars-Last-Digit-of-a-Large-Number/LastDigit.cs
@@ -1,35 +1,39 @@
 ﻿using System.Numerics;
 
-namespace CodeWars_Last_Digit_of_a_Large_Number
+namespace CodeWars_Last_Digit_of_a_Large_Number;
+
+public class LastDigit
 {
-    public class LastDigit
+    public static int GetLastDigit(BigInteger n1, BigInteger n2)
     {
-        public static int GetLastDigit(BigInteger n1, BigInteger n2)
+        //**** Clever Solution **** => return (int)BigInteger.ModPow(n1, n2, 10);
+        // Wait, Whaaaaaaat??? HOW???!
+            
+        var n1Str= n1.ToString();
+        var n2Str= n2.ToString();
+            
+        if (n1Str.Length == 1 & n1Str[0] == '0' & n2Str.Length == 1 & n1Str[0] == '0') 
+            return 1;
+        if (n1Str.Length == 1 & n1Str[0] == '0')
+            return 0;
+        if (n2Str.Length == 1 & n2Str[0] == '0')
+            return 1;
+            
+        var numberN1 = n1Str[n1.ToString().Length - 1] - '0';
+        var numberN2 = 0;
+            
+        foreach (var digit in n2Str)
         {
-            //**** Clever Solution **** => return (int)BigInteger.ModPow(n1, n2, 10);
-            // HOW???!
-            var n1Str= n1.ToString();
-            var n2Str= n2.ToString();
-            if (n1Str.Length == 1 & n1Str[0] == '0' & n2Str.Length == 1 & n1Str[0] == '0') return 1;
-            if (n1Str.Length == 1 & n1Str[0] == '0')
-                return 0;
-            if (n2Str.Length == 1 & n2Str[0] == '0')
-                return 1;
-            var numberN1 = n1Str[n1.ToString().Length - 1] - '0';
-            var numberN2 = 0;
-            foreach (var digit in n2Str)
-            {
-                numberN2 = numberN2 * 10 + (digit - '0');
-                numberN2 %= 4;
-            }
-
-            if (numberN2 == 0) numberN2 = 4;
-            var lastDigit = Math.Pow(numberN1, numberN2);
-            lastDigit %= 10;
-            return (int)lastDigit;
-
+            numberN2 = numberN2 * 10 + (digit - '0');
+            numberN2 %= 4;
         }
-    
-  
+
+        if (numberN2 == 0)
+            numberN2 = 4;
+            
+        var lastDigit = Math.Pow(numberN1, numberN2);
+        lastDigit %= 10;
+            
+        return (int)lastDigit;
     }
 }
